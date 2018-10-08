fcc1b5a7392f7974ead2447c405155ac7bd2e013
Christopher Seay <36781577+astroseay@users.noreply.github.com>
2018-10-08 19:30:19 +0200

    implement loglog and pcsa

--- 2/a2.py
+++ 2/a2.py
@@ -12,9 +12,11 @@ import time
 
 
 def trailing_zeroes(num):
-    """Counts the number of trailing 0 bits in num."""
+    """
+    Counts the number of trailing 0 bits in hashed value 'num'
+    """
     if num == 0:
-        return 32
+        return 0 #return 32 or 0?
     p = 0
     while (num >> p) & 1 == 0:
         p += 1
@@ -23,23 +25,30 @@ def trailing_zeroes(num):
 
 def LogLog(values, k):
     """
-    estimates the number of unique elements in the input set values.
+    estimates the number of unique elements in the input set values
+    using the durand-flajolet (2003) algorithm.
 
     arguments:
-        values: an iterator of hashable elements to estimate the cardinality of.
-        k: the number of bits of hash to use as a bucket number; there will be 2**k buckets.
+        values: an iterator of hashable elements to estimate the cardinality of
+        k: the number of bits of hash to use as a bucket number
+
+    parameters:
+        m: 2**k buckets
+        vals: to find out
+        M_zeroes: M-elements of data stream, 1 for each bucket
     """
-    vals=[np.random.randint(0,2**31) for i in range(values)]  #values to be hashed
-    num_buckets = 2 ** k
-    max_zeroes = [0] * num_buckets
+    vals = [np.random.randint(0,2**30) for i in range(values)]  #values to be hashed
+    m = 2**k
+    M_zeroes = [0]*m # initialize M^(1),...,M^(m) to 0
+    print(M_zeroes)
     for value in np.arange(values):
         h = vals[value]
-        bucket = h & (num_buckets - 1) # Mask out the k least significant bits as bucket ID
+        bucket = h & (m - 1) # Mask out the k least significant bits as bucket ID
         h_value= h >> k
         #print bin(h_value)
-        max_zeroes[bucket] = max(max_zeroes[bucket], trailing_zeroes(h_value))
+        M_zeroes[bucket] = max(M_zeroes[bucket], trailing_zeroes(h_value))
     #print('estimated value:',2 ** (float(sum(max_zeroes)) / num_buckets) * num_buckets * 0.79402)
-    return 2 ** (float(sum(max_zeroes)) / num_buckets) * num_buckets * 0.79402
+    return(2**(float(sum(M_zeroes))/m)*m*0.79402)
 
 
 def pcsa(values):
@@ -51,7 +60,7 @@ def pcsa(values):
     num_hash=2**7 #number of hash functions to be tested
     R_list=[0]*num_hash  #list of highest number of trailing zeroes
     for h in range(num_hash):
-        k=30  #number of bits for each hash value
+        k=32  #number of bits for each hash value
         hash_vals = np.matrix([[np.random.randint(0, 1) for i in range(k)] for j in range(values)]) #randomy generated hash values
         R=0
         for value in np.arange(values):
@@ -69,58 +78,12 @@ def pcsa(values):
 
 if __name__ == '__main__':
     np.random.seed(1)
-    #print ('Relative Approximation Errors for the 3 algorithms.')
-    num_unique = np.random.randint(1,2**8) #
-    print ('true n:',num_unique)
+    num_unique = np.random.randint(1,2e4) #
+    # print(np.size(num_unique))
+    # print('true n:',num_unique)
     start_time = time.time()
-    print ('PCSA: %.3f'% np.abs(1-np.mean([(pcsa(num_unique)/num_unique) for j in range(1)])))
+    print('PCSA: %.3f'% np.abs(1-np.mean([(pcsa(num_unique)/num_unique) for j in range(1)])))
     print('Run time %s seconds'  % (time.time() - start_time))
     start_time = time.time()
-    print ('LogLog: %.3f' % ((np.abs(1-np.mean([LogLog(num_unique, 20)/num_unique for j in range(1)])))))
+    print('LogLog: %.3f' % ((np.abs(1-np.mean([LogLog(num_unique, 20)/num_unique for j in range(1)])))))
     print('Run time %s seconds'  % (time.time() - start_time))
-
-
-'''
-import random
-
-def randomHash(modulus):
-   a, b = random.randint(0,modulus-1), random.randint(0,modulus-1)
-   def f(x):
-      return (a*x + b) % modulus
-   return f
-
-def average(L):
-   return sum(L) / len(L)
-
-def numDistinctElements(stream, numParallelHashes=10):
-   modulus = 2**20
-   hashes = [randomHash(modulus) for _ in range(numParallelHashes)]
-   minima = [modulus] * numParallelHashes
-   currentEstimate = 0
-
-   for i in stream:
-      hashValues = [h(i) for h in hashes]
-      for i, newValue in enumerate(hashValues):
-         if newValue < minima[i]:
-            minima[i] = newValue
-
-      currentEstimate = modulus / average(minima)
-
-      yield currentEstimate
-
-
-S = [random.randint(1,2**20) for _ in range(10000)]
-
-for k in range(10,301,10):
-   for est in numDistinctElements(S, k):
-      pass
-   print(abs(est))
-
-
-S = range(10000) #[random.randint(1,2**20) for _ in range(10000)]
-
-for k in range(10,301,10):
-   for est in numDistinctElements(S, k):
-      pass
-   print(abs(est))
-'''
--- 2/jhash.py
+++ 2/jhash.py
@@ -102,3 +102,48 @@ if __name__ == "__main__":
     hashstr = 'My hovercraft is full of eels'
     myhash = jhash(hashstr)
     print ('jhash("%s"): %d' % (hashstr, myhash))
+
+    '''
+    import random
+
+    def randomHash(modulus):
+       a, b = random.randint(0,modulus-1), random.randint(0,modulus-1)
+       def f(x):
+          return (a*x + b) % modulus
+       return f
+
+    def average(L):
+       return sum(L) / len(L)
+
+    def numDistinctElements(stream, numParallelHashes=10):
+       modulus = 2**20
+       hashes = [randomHash(modulus) for _ in range(numParallelHashes)]
+       minima = [modulus] * numParallelHashes
+       currentEstimate = 0
+
+       for i in stream:
+          hashValues = [h(i) for h in hashes]
+          for i, newValue in enumerate(hashValues):
+             if newValue < minima[i]:
+                minima[i] = newValue
+
+          currentEstimate = modulus / average(minima)
+
+          yield currentEstimate
+
+
+    S = [random.randint(1,2**20) for _ in range(10000)]
+
+    for k in range(10,301,10):
+       for est in numDistinctElements(S, k):
+          pass
+       print(abs(est))
+
+
+    S = range(10000) #[random.randint(1,2**20) for _ in range(10000)]
+
+    for k in range(10,301,10):
+       for est in numDistinctElements(S, k):
+          pass
+       print(abs(est))
+    '''
