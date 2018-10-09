2cf961a1a3026cd7bc4e6816dfcc89c5a0211147
Christopher Seay <36781577+astroseay@users.noreply.github.com>
2018-10-09 20:21:13 +0200

    a2.py (almost) final

--- 2/a2.py
+++ 2/a2.py
@@ -11,15 +11,29 @@ import random
 import math
 import time
 
-def relError(size,cardinality):
+def getSeed():
+    """
+    generate random stream of 32-bit numbers
+
+    """
+    n = int(1e6)
+    a = []
+    for x in range(1,n):
+        a.append(random.getrandbits(32))
+    return a
+def rae(size,cardinality):
+    """
+    relative approximate error
+    """
     return np.abs(size-cardinality)/size
 
 def trailing_zeroes(num):
     """
     Counts the number of trailing 0 bits in hashed value 'num'
     """
+    # print(num)
     if num == 0:
-        return 0 #return 32 or 0?
+        return 32 # assuming 32bit inputs
     p = 0
     while (num >> p) & 1 == 0:
         p += 1
@@ -42,12 +56,12 @@ def LogLog(vals,k):
     alpha = 0.79402
     m = 2**k
     M_zeroes = [0]*m # initialize M^(1),...,M^(m) to 0
-    #print(M_zeroes)
+    # print(M_zeroes)
     for value in vals:
         h = hash(value)
         bucket = h & (m - 1) # mask out the k least significant bits as bucket ID
         h_value= h >> k
-        #print bin(h_value)
+        # print bin(h_value)
         M_zeroes[bucket] = max(M_zeroes[bucket], trailing_zeroes(h_value))
     return(2**(float(sum(M_zeroes))/m)*m*alpha)
 
@@ -57,7 +71,13 @@ def pcsa(values):
     probabilistic counting with stochastic averaging. algorithm described
     in flajolet & martin (1985).
     '''
-    R = trailing_zeroes(values)
+    R = 0
+    max_zeroes = []
+    for val in values:
+        # print(val,type(val))
+        if trailing_zeroes(val) > R:
+            R = trailing_zeroes(val)
+    print('cardinality estimate: %d' % R)
     return 2**R
     # m = 2**10 #number of hash functions to be tested
     # phi = 0.77351 #magic number phi
@@ -81,20 +101,29 @@ def pcsa(values):
 """
 iterating k buckets (between 64 bits ie 2^4 and 1024 bits 2^10)
 and over m number of buckets in same range,
-over n size.
+over n size (10000).
 """
 if __name__ == '__main__':
-    # np.random.seed(1)
-    vals = [random.getrandbits(32) for i in range(10000)]
-    #num_unique = np.random.randint(1,2e4) #
-    # print(np.size(num_unique))
-    # print('true n:',num_unique)
-    #start_time = time.time()
-    # pcsa_test=[np.abs(10000-pcsa(vals)/10000) for j in range(1)]
-    #print('PCSA: %.3f'% np.abs(1-np.mean([(pcsa(num_unique)/num_unique) for j in range(1)])))
-    #print('Run time %s seconds'  % (time.time() - start_time))
-    #start_time = time.time()
-    test = [np.abs(10000-LogLog(vals,10)/10000) for j in range(1)]
-    print(test)
+    n = int(1e6) # size of data stream
+    # stream = []
+    # for i in range(1):
+    #     stream.append(getSeed())
+    #     np.array(stream)
+    
+    stream = [random.getrandbits(32) for i in range(n)] # randomly generated values
+    # print('true n:',np.unique(stream))
+    start_time = time.time()
+    pcsa_run = np.array([pcsa(stream) for j in range(1)])
+    pcsa_rae = rae(n,pcsa_run)
+    print('Run time: %s seconds'  %(time.time() - start_time))
+    print('Relative approximate error: %.3f' % pcsa_rae)
+    # print(pcsa_run,pcsa_rae)
+
+    start_time = time.time()
+    loglog_run = np.array([LogLog(stream,10) for j in range(1)])
+    loglog_rae = rae(n,loglog_run)
+    print('Run time: %s seconds'  %(time.time() - start_time))
+    print('Relative approximate error: %.3f' % loglog_rae)
+    #print(loglog_run)
     #print('LogLog: %.3f' % ((np.abs(10000-LogLog(vals,5)/10000) for j in range(1))))
     #print('Run time %s seconds'  % (time.time() - start_time))
